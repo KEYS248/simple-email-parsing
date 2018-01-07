91c518fc0e9b265d7a4f7c91474c56a38999f0a6
David Klein <kleinjdavid@gmail.com>
2018-01-07 11:19:37 -0800

    simple email reading upload

---
+++ email-reader.py
@@ -0,0 +1,39 @@
+import email
+import imaplib
+import smtpd
+import time
+
+account = 'yourEmail@gmail.com'
+password = 'yourPassword'
+		# Gmail users will have to allow access to less secure apps from this link.
+		#    https://myaccount.google.com/lesssecureapps
+		# Gmail users with 2-Step Verification will have to generate a one-time App password.
+		#    Simple follow the link above and click the lower 'Learn More' link.
+smtp_server = 'imap.gmail.com'
+smtp_port = 993
+
+def readmail():
+	try:
+		mail = imaplib.IMAP4_SSL(smtp_server)
+		mail.login(account, password)
+
+		mail.select('inbox')
+		type, data = mail.search(None, 'ALL')
+		mail_ids = data[0]
+		id_list = mail_ids.split()
+
+		for i in reversed(id_list):
+			type, data = mail.fetch(i, '(RFC822)')
+
+			for response_part in data:
+				if isinstance(response_part, tuple):
+					msg = email.message_from_string(response_part[1].decode('utf-8', errors='ignore'))
+					email_subject = msg['subject']
+					email_from = msg['from']
+					print('From: ' + email_from)
+					print('Subject: ' + email_subject + '\n')
+
+	except Exception as e:
+		print('ERROR: ' + str(e))
+
+readmail()
